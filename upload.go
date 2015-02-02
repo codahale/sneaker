5cbe57c61d2284c46acfa18dd9da9a4b3be5696e
Coda Hale <coda@stripe.com>
2015-02-02 13:20:05 -0800

    Added Upload method to Manager.

---
+++ upload.go
@@ -0,0 +1,62 @@
+package secman
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	fpath "path"
+
+	"github.com/awslabs/aws-sdk-go/aws"
+	"github.com/awslabs/aws-sdk-go/gen/kms"
+	"github.com/awslabs/aws-sdk-go/gen/s3"
+)
+
+// Upload encrypts the given secret with a KMS data key and uploads it to S3.
+func (m *Manager) Upload(path string, r io.Reader) error {
+	path = fpath.Join(m.Prefix, path)
+
+	secret, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+
+	key, err := m.Keys.GenerateDataKey(&kms.GenerateDataKeyRequest{
+		KeyID:         &m.KeyID,
+		NumberOfBytes: aws.Integer(32),
+	})
+	if err != nil {
+		return err
+	}
+
+	ciphertext, err := encrypt(key.Plaintext, secret)
+	if err != nil {
+		return err
+	}
+
+	if _, err := m.Objects.PutObject(
+		&s3.PutObjectRequest{
+			ContentLength: aws.Long(int64(len(key.CiphertextBlob))),
+			ContentType:   aws.String(contentType),
+			Bucket:        aws.String(m.Bucket),
+			Key:           aws.String(path + ".kms"),
+			Body:          ioutil.NopCloser(bytes.NewReader(key.CiphertextBlob)),
+		},
+	); err != nil {
+		return err
+	}
+
+	if _, err := m.Objects.PutObject(
+		&s3.PutObjectRequest{
+			ContentLength: aws.Long(int64(len(ciphertext))),
+			ContentType:   aws.String(contentType),
+			Bucket:        aws.String(m.Bucket),
+			Key:           aws.String(path + ".aes"),
+			Body:          ioutil.NopCloser(bytes.NewReader(ciphertext)),
+		},
+	); err != nil {
+		return err
+	}
+	return nil
+}
+
+const contentType = "application/octet-stream"
---
+++ upload_test.go
@@ -0,0 +1,112 @@
+package secman
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/awslabs/aws-sdk-go/gen/kms"
+	"github.com/awslabs/aws-sdk-go/gen/s3"
+)
+
+func TestUpload(t *testing.T) {
+	fakeKMS := &FakeKMS{
+		GenerateResponses: []kms.GenerateDataKeyResponse{
+			{
+				Plaintext:      make([]byte, 32),
+				CiphertextBlob: []byte("encrypted key"),
+			},
+		},
+	}
+
+	fakeS3 := &FakeS3{
+		PutResponses: []s3.PutObjectOutput{
+			{},
+			{},
+		},
+	}
+
+	man := Manager{
+		Objects: fakeS3,
+		Keys:    fakeKMS,
+		Bucket:  "bucket",
+		Prefix:  "secrets",
+		KeyID:   "key1",
+	}
+
+	if err := man.Upload("weeble.txt", strings.NewReader("this is a test")); err != nil {
+		t.Fatal(err)
+	}
+
+	// KMS request
+
+	genReq := fakeKMS.GenerateRequests[0]
+	if v, want := *genReq.KeyID, "key1"; v != want {
+		t.Errorf("Key ID was %q, but expected %q", v, want)
+	}
+
+	if v, want := *genReq.NumberOfBytes, 32; v != want {
+		t.Errorf("Key size was %d, but expected %d", v, want)
+	}
+
+	// key upload
+
+	putReq := fakeS3.PutRequests[0]
+	if v, want := *putReq.Bucket, "bucket"; v != want {
+		t.Errorf("Bucket was %q, but expected %q", v, want)
+	}
+
+	if v, want := *putReq.Key, "secrets/weeble.txt.kms"; v != want {
+		t.Errorf("Key was %q, but expected %q", v, want)
+	}
+
+	if v, want := *putReq.ContentLength, int64(13); v != want {
+		t.Errorf("ContentLength was %d, but expected %d", v, want)
+	}
+
+	if v, want := *putReq.ContentType, "application/octet-stream"; v != want {
+		t.Errorf("ContentType was %q, but expected %q", v, want)
+	}
+
+	encDataKey, err := ioutil.ReadAll(putReq.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if v, want := string(encDataKey), "encrypted key"; v != want {
+		t.Errorf("Encrypted key was %q, but expected %q", v, want)
+	}
+
+	// secret upload
+
+	putReq = fakeS3.PutRequests[1]
+	if v, want := *putReq.Bucket, "bucket"; v != want {
+		t.Errorf("Bucket was %q, but expected %q", v, want)
+	}
+
+	if v, want := *putReq.Key, "secrets/weeble.txt.aes"; v != want {
+		t.Errorf("Key was %q, but expected %q", v, want)
+	}
+
+	if v, want := *putReq.ContentLength, int64(42); v != want {
+		t.Errorf("ContentLength was %d, but expected %d", v, want)
+	}
+
+	if v, want := *putReq.ContentType, "application/octet-stream"; v != want {
+		t.Errorf("ContentType was %q, but expected %q", v, want)
+	}
+
+	encSecret, err := ioutil.ReadAll(putReq.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	secret, err := decrypt(make([]byte, 32), encSecret)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if v, want := string(secret), "this is a test"; v != want {
+		t.Errorf("Decrypted key was %q, but expected %q", v, want)
+	}
+}
