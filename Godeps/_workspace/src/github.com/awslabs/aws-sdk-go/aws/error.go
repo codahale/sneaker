20a3945d8ceb9a7d832184ed3ecd3a4b4cd0b01e
Coda Hale <coda.hale@gmail.com>
2015-04-18 13:32:57 -0700

    Update dependencies.

--- Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/aws/error.go
+++ Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/aws/error.go
@@ -3,14 +3,21 @@ package aws
 // An APIError is an error returned by an AWS API.
 type APIError struct {
 	StatusCode int // HTTP status code e.g. 200
-	Type       string
 	Code       string
 	Message    string
 	RequestID  string
-	HostID     string
-	Specifics  map[string]string
 }
 
 func (e APIError) Error() string {
-	return e.Message
+	return e.Code + ": " + e.Message
+}
+
+func Error(e error) *APIError {
+	if err, ok := e.(*APIError); ok {
+		return err
+	} else if err, ok := e.(APIError); ok {
+		return &err
+	} else {
+		return nil
+	}
 }
--- Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/aws/types.go
+++ Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/aws/types.go
@@ -1,94 +1,63 @@
 package aws
 
 import (
-	"math"
-	"strconv"
+	"io"
 	"time"
 )
 
-// A StringValue is a string which may or may not be present.
-type StringValue *string
-
-// String converts a Go string into a StringValue.
-func String(v string) StringValue {
+// String converts a Go string into a string pointer.
+func String(v string) *string {
 	return &v
 }
 
-// A BooleanValue is a boolean which may or may not be present.
-type BooleanValue *bool
-
-// Boolean converts a Go bool into a BooleanValue.
-func Boolean(v bool) BooleanValue {
+// Boolean converts a Go bool into a boolean pointer.
+func Boolean(v bool) *bool {
 	return &v
 }
 
-// True is the BooleanValue equivalent of the Go literal true.
-func True() BooleanValue {
-	return Boolean(true)
-}
-
-// False is the BooleanValue equivalent of the Go literal false.
-func False() BooleanValue {
-	return Boolean(false)
-}
-
-// An IntegerValue is an integer which may or may not be present.
-type IntegerValue *int
-
-// Integer converts a Go int into an IntegerValue.
-func Integer(v int) IntegerValue {
+// Long converts a Go int64 into a long pointer.
+func Long(v int64) *int64 {
 	return &v
 }
 
-// A LongValue is a 64-bit integer which may or may not be present.
-type LongValue *int64
-
-// Long converts a Go int64 into a LongValue.
-func Long(v int64) LongValue {
+// Double converts a Go float64 into a double pointer.
+func Double(v float64) *float64 {
 	return &v
 }
 
-// A FloatValue is a 32-bit floating point number which may or may not be
-// present.
-type FloatValue *float32
-
-// Float converts a Go float32 into a FloatValue.
-func Float(v float32) FloatValue {
-	return &v
+// Time converts a Go Time into a Time pointer
+func Time(t time.Time) *time.Time {
+	return &t
 }
 
-// A DoubleValue is a 64-bit floating point number which may or may not be
-// present.
-type DoubleValue *float64
-
-// Double converts a Go float64 into a DoubleValue.
-func Double(v float64) DoubleValue {
-	return &v
+func ReadSeekCloser(r io.Reader) ReaderSeekerCloser {
+	return ReaderSeekerCloser{r}
 }
 
-// A UnixTimestamp is a Unix timestamp represented as fractional seconds since
-// the Unix epoch.
-type UnixTimestamp struct {
-	Time time.Time
+type ReaderSeekerCloser struct {
+	r io.Reader
 }
 
-// MarshalJSON marshals the timestamp as a float.
-func (t UnixTimestamp) MarshalJSON() (text []byte, err error) {
-	n := float64(t.Time.UnixNano()) / 1e9
-	s := strconv.FormatFloat(n, 'f', -1, 64)
-	return []byte(s), nil
-}
-
-// UnmarshalJSON unmarshals the timestamp from a float.
-func (t *UnixTimestamp) UnmarshalJSON(text []byte) error {
-	f, err := strconv.ParseFloat(string(text), 64)
-	if err != nil {
-		return err
+func (r ReaderSeekerCloser) Read(p []byte) (int, error) {
+	switch t := r.r.(type) {
+	case io.Reader:
+		return t.Read(p)
 	}
+	return 0, nil
+}
 
-	sec := math.Floor(f)
-	nsec := (f - sec) * 1e9
+func (r ReaderSeekerCloser) Seek(offset int64, whence int) (int64, error) {
+	switch t := r.r.(type) {
+	case io.Seeker:
+		return t.Seek(offset, whence)
+	}
+	return int64(0), nil
+}
 
-	t.Time = time.Unix(int64(sec), int64(nsec)).UTC()
+func (r ReaderSeekerCloser) Close() error {
+	switch t := r.r.(type) {
+	case io.Closer:
+		return t.Close()
+	}
 	return nil
 }
